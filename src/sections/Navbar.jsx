4eb5965409c3583e2a9ddb0338df3a89c8d73c0b
MadhushanTharanga <madhushan0617@gmail.com>
2025-03-21 23:23:00 +0530

    updated!

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = ()=>setIsOpen((prevIsOpen) => !prevIsOpen);
     return (
         <header className="fixed top-0 left-10 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto ">
@@ -8,6 +10,9 @@ function Navbar() {
                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                         Madhushan
                     </a>
+                    <button onClick={toggleMenu}>
+                        <img src={isOpen? "assets/close.svg":"assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+                    </button>
                 </div>
             </div>
         </header>
