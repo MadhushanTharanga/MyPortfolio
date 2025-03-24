0dbf468a213ba74daa2e4bc52c5f0ed6903d6e0b
MadhushanTharanga <madhushan0617@gmail.com>
2025-03-25 00:12:17 +0530

    completed portfolio

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      <div className="text-white-500 flex gap-2">
+        <p>Terms & Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+
+      <div className="flex gap-3">
+        <div className="social-icon">
+          <a href="https://github.com/MadhushanTharanga" className="w-1/2 h-1/2">
+          <img src="/assets/github.svg" alt="github" />
+          </a>
+        </div>
+        <div className="social-icon">
+          <a href="https://www.linkedin.com/in/madhushan-tharanga/" className="w-1/2 h-1/2">
+          <img src="/assets/linkedin.png" alt="linkedin" />
+          </a>
+        </div>
+      </div>
+      <p className="text-white-500">© 2025 Madhushan Tharanga. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
